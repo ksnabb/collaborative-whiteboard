cb145232d080d670fc948b9ad09952aa9eb121b0
ksnabb <kristoffer.snabb@geonition.org>
2014-11-27 22:24:20 +0200

    initial commit with server and gulp and npm pakcage.json file

---
+++ app.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func eventHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+
+	// listen to messages
+
+	// serve the static files
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Print(err)
+	}
+	mux := mux.NewRouter()
+	mux.HandleFunc("/api", eventHandler)
+	mux.Handle("/", http.FileServer(http.Dir(dir+"/public")))
+
+	http.Handle("/", mux)
+
+	log.Print("listening on port 7000")
+	http.ListenAndServe(":7000", nil)
+}
